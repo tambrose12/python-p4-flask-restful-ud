49e4412e240192a02c833ed0b89d4a3b64734db5
Taylor Ambrose <taykesby@gmail.com>
2023-04-08 18:24:27 -0400

    lab complete

---
+++ server/migrations/versions/3eb301ecd5f6_first_run.py
@@ -0,0 +1,24 @@
+"""first run
+
+Revision ID: 3eb301ecd5f6
+Revises: 2da2c0ccb068
+Create Date: 2023-04-08 18:10:33.223463
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3eb301ecd5f6'
+down_revision = '2da2c0ccb068'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
